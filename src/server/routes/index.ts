d328b67a276f2e68fac566b7ad68aeeeda4d5b5d
Alexandre Castelo Branco Brasileiro <37881265+alexandrecbb@users.noreply.github.com>
2023-09-25 17:08:10 -0300

    Corrigindo atualização do yup

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,6 +8,7 @@ router.get('/', (_, res) => {
     return res.send('Rodando!');
 });
 
+router.get('/cities', CitiesController.getAllValidation, CitiesController.getAll);
 router.post('/cities', CitiesController.createValidation, CitiesController.create);
 
 
--- src/server/shared/middleware/Validation.ts
+++ src/server/shared/middleware/Validation.ts
@@ -1,12 +1,12 @@
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { Schema, ValidationError } from 'yup';
+import { AnyObject, Maybe, ObjectSchema, ValidationError } from 'yup';
 
 type TProperty = 'body' | 'header' | 'params' | 'query';
 
-type TGetSchema = <T>(schema: Schema<T>) => Schema<T>;
+type TGetSchema = <T extends Maybe<AnyObject>>(schema: ObjectSchema<T>) => ObjectSchema<T>;
 
-type TAllSchemas = Record<TProperty, Schema<any>>;
+type TAllSchemas = Record<TProperty, ObjectSchema<any>>;
 
 type TGetAllSchemas = (getSchema: TGetSchema) => Partial<TAllSchemas>;
 
@@ -21,7 +21,6 @@ export const validation: TValidation = (getAllSchemas) => async (req, res, next)
     Object.entries(schemas).forEach(([key, schema]) => {
         try {
             schema.validateSync(req[key as TProperty], { abortEarly: false });
-            // return next();
         } catch (err) {
             const yupError = err as ValidationError;
             const errors: Record<string, string> = {};
