2f8befd8b5cfc62239fee81ff4238df959bafb54
Alexandre Castelo Branco Brasileiro <37881265+alexandrecbb@users.noreply.github.com>
2023-10-01 23:28:01 -0300

    Criando testes das rotas

--- src/server/controllers/cities/GetAll.ts
+++ src/server/controllers/cities/GetAll.ts
@@ -6,22 +6,27 @@ import { StatusCodes } from 'http-status-codes';
 interface IQueryProps {
     page?: number;
     limit?: number;
-    filter?: string;  
+    filter?: string;
 }
 
 export const getAllValidation = validation((getSchema) => ({
     query: getSchema<IQueryProps>(yup.object().shape({
-        page: yup.number().optional().moreThan(0), 
+        page: yup.number().optional().moreThan(0),
         limit: yup.number().optional().moreThan(0),
-        filter: yup.string().optional(),     
+        filter: yup.string().optional(),
     })),
-    
+
 }));
 
 export const getAll = async (req: Request<{}, {}, {}, IQueryProps>, res: Response) => {
 
-    console.log(req.query);
+    res.setHeader('access-control-expose-headers', 'x-total-count');
+    res.setHeader('x-total-count', 1);
 
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado!');
-
+    return res.status(StatusCodes.OK).json([
+        {
+            id: 1,
+            name: 'Caxias do Sul',
+        }
+    ]);
 };
