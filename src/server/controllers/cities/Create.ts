b87e7b88e297d02475328717c822f4115d58a674
Alexandre Castelo Branco Brasileiro <37881265+alexandrecbb@users.noreply.github.com>
2023-10-23 22:17:08 -0300

    finalizando provider de city

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -3,6 +3,7 @@ import * as yup from 'yup';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import { CitiesProvider } from '../../database/providers/cities';
 import { validation } from '../../shared/middleware';
 import { ICity } from '../../database/models';
 
@@ -10,13 +11,23 @@ interface IBodyProps extends Omit<ICity, 'id'> {}
 
 export const createValidation = validation((getSchema) => ({
     body: getSchema<IBodyProps>(yup.object().shape({
-        name: yup.string().required().min(3),      
+        name: yup.string().required().min(3).max(150),      
     })),
     
 }));
 
 export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
 
-    return res.status(StatusCodes.CREATED).json(1);
+    const result = await CitiesProvider.create(req.body);
+
+    if(result instanceof Error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+            { errors: { 
+                default: result.message } 
+            }
+        );
+    }
+
+    return res.status(StatusCodes.CREATED).json(result);
 
 };
