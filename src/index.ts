576874c7ecb75d9c2dbb78aa9da976ed45449095
Alexandre Castelo Branco Brasileiro <37881265+alexandrecbb@users.noreply.github.com>
2023-10-26 20:41:55 -0300

    Resolvendo bugs de produção.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
 import { server } from './server/Sever';
+import { Knex } from './server/database/knex';
 
-server.listen(process.env.PORT || 3333, () => {
-    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
-});
+
+const startServer = () => {
+    server.listen(process.env.PORT || 3333, () => {
+        console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+    });
+};
+
+
+if (process.env.IS_LOCALHOST !== 'true') {
+
+    Knex.migrate.latest()
+        .then(() => {
+            startServer();
+        }).catch((error) => {
+            console.log(error);
+        });
+
+} else {
+    startServer();
+}
