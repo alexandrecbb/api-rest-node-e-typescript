471dc0abafeb0ff149e58110eae2d9501ca30e54
Alexandre Castelo Branco Brasileiro <37881265+alexandrecbb@users.noreply.github.com>
2023-11-07 22:23:15 -0300

    Criando providers para usuários

---
+++ src/server/database/providers/users/Create.ts
@@ -0,0 +1,26 @@
+import { ETableNames } from '../../ETableNames';
+import { Knex } from '../../knex';
+import { IUser } from '../../models';
+
+export const create = async (user: Omit<IUser, 'id'>) : Promise<number | Error> => {
+   
+    try {
+        
+        const [result] = await Knex(ETableNames.user).insert(user).returning('id');
+
+        if(typeof result === 'object') {
+            return result.id;
+        }else if (typeof result === 'number') {
+            return result;
+        }
+
+        return new Error('Error ao cadastrar registro');
+         
+    } catch (error) {
+
+        console.log(error);
+        return Error('Error ao cadastrar registro');
+
+    }
+    
+};
