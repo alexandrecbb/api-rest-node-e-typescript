cd3e4b95cb1a0f5e30bb1d8bcc449bd8bc055684
Alexandre Castelo Branco Brasileiro <37881265+alexandrecbb@users.noreply.github.com>
2023-11-26 07:46:52 -0300

    Novas variáveis de ambiente

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,6 +1,14 @@
 import { knex } from 'knex';
+import 'dotenv/config';
+import pg from 'pg';
 
 import { development, production, test } from './Environment';
+
+
+if (process.env.NODE_ENV === 'production') {
+    pg.types.setTypeParser(20, 'text', parseInt);
+
+}
 
 const geEnvironment = () => {
     switch (process.env.NODE_ENV) {
